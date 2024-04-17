c4fff3e6d2ee6451dd3d0fa76b1e83373b0c537a
Hayzed-codes <raheemhorlah@gmail.com>
2024-04-17 15:40:59 +0100

    Update

---
+++ frontend/src/Component/Register/AdminReg.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Register.css'
+
+const AdminReg = () => {
+  return (
+    <div className='container form__ --100vh'>
+      
+    </div>
+  )
+}
+
+export default AdminReg
