32b3ed6bd8648fee05d63a5f8d32d47b23eccc46
Hayzed-codes <raheemhorlah@gmail.com>
2024-04-23 11:40:16 +0100

    Update

--- frontend/src/Component/Dashboard/Sidebar.jsx
+++ frontend/src/Component/Dashboard/Sidebar.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    const Sidebar = () => {
-      const [activeIndex, setActiveIndex] = useState(0);
-    
-      const handleLinkClick = (index) => {
-        setActiveIndex(index);
-      };
-    }
-    
-    const dashboardLinks = [
-        { title: "Students", url: "/studentdash" },
-        { title: "Rooms", url: "/room" },
-        { title: "RoomType", url: "/room-type" },
-        { title: "Reports", url: "/report" },
-      ];
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    
+  const handleLinkClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const dashboardLinks = [
+    { title: "Students", url: "/studentdash" },
+    { title: "Rooms", url: "/room" },
+    { title: "RoomType", url: "/room-type" },
+    { title: "Reports", url: "/report" },
+  ];
+
   return (
-    
-    <div className='--flex-start'>
+    <div className="--flex-start">
       <div className="left">
-        {dashboardLinks.map(({title, url}, i) => (
-            <div  className='--flex-center --dir-column' key={i}>
-                <Link to={url}>
-                    {title}
-                </Link>
-            </div>
+        {dashboardLinks.map(({ title, url }, index) => (
+          <div className="--flex-center --dir-column" key={index}>
+            <Link
+              to={url}
+              className={index === activeIndex ? "active-link" : ""}
+              onClick={() => handleLinkClick(index)}
+            >
+              {title}
+            </Link>
+          </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- frontend/src/Component/Dashboard/StudentDashboard.jsx
+++ frontend/src/Component/Dashboard/StudentDashboard.jsx
@@ -1,13 +1,63 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import './StudentDashboard.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+const studentsData = [
+    {
+        id: 1,
+        name: "Jon Doe",
+        email: "Jon_Doe@outlook.com",
+        idNumber: "12345",
+        gender: "Female",
+        age: 23,
+        nationality: "American"
+    },
+    {
+        id: 2,
+        name: "Michelle Lilian",
+        email: "bigbunda@gmail.com",
+        idNumber: "12345",
+        gender: "Female",
+        age: 18,
+        nationality: "Isrealite"
+    },
+    {
+        id: 3,
+        name: "olabode",
+        email: "nitroeef@yahoo.com",
+        idNumber: "12345",
+        gender: "Male",
+        age: 26,
+        nationality: "Nigerian"
+    },
+]
 
 const StudentDashboard = () => {
+        const [searchTerm, setSearchTerm] = useState('');
+        const [students, setStudents] = useState(studentsData);
+        const [filteredData, setFilteredData] = useState(studentsData);
+        
+
+        const handleSearchChange = (e) => {
+            const term = e.target.value.toLowerCase()
+            const filtered = studentsData.filter(
+                (student) =>
+                student.name.toLowerCase().includes(term) ||
+                student.email.toLowerCase().includes(term)
+            );
+            setFilteredData(filtered)
+        }
+
     
   return (
     <div>
         <Sidebar />
       <h1>Student</h1>
+
+      {/* {studentsData.map(({id, name, email, idNumber, gender, age, nationality}, i) => )} */}
     </div>
   )
 }
