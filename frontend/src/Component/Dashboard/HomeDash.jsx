e7348048f928aca5e46f2d60071b1b8daf0b7e3c
Hayzed-codes <raheemhorlah@gmail.com>
2024-06-03 10:10:24 +0100

    Ammend

--- frontend/src/Component/Dashboard/HomeDash.jsx
+++ frontend/src/Component/Dashboard/HomeDash.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./HomeDash.css";
 import { blacky, lady, lady2, tallest } from "../../assets";
 import { Link } from "react-router-dom";
-
 
 const HomeDash = () => {
   const activities = [
@@ -65,80 +64,6 @@ const HomeDash = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className="--flex-center __firstCon">
-        <h4 className="__title">Recent Activity</h4>
-
-        <div className="__users">
-          <div className="__firstUserPic">
-            <img src={lady} alt="lady-picture" />
-          </div>
-
-          <div className="__userData">
-            <div>
-              <h5>Tunmise</h5>
-
-              <p>Tunmise has been checked out</p>
-            </div>
-
-            <p>Now</p>
-          </div>
-
-        </div>
-
-        <div className="__users">
-          <div className="__firstUserPic">
-            <img src={lady2} alt="lady-picture" />
-          </div>
-
-          <div className="__userData">
-            <div>
-              <h5>Kenny</h5>
-
-              <p>Kenny has been checked out</p>
-            </div>
-
-            <p>3 mins ago</p>
-          </div>
-
-        </div>
-
-        <div className="__users">
-          <div className="__firstUserPic">
-            <img src={blacky} alt="lady-picture" />
-          </div>
-
-          <div className="__userData">
-            <div>
-              <h5>Bode</h5>
-
-              <p>Bode has been checked out</p>
-            </div>
-
-            <p>5 mins ago</p>
-          </div>
-
-        </div>
-
-        <div className="__users">
-          <div className="__firstUserPic">
-            <img src={tallest} alt="lady-picture" />
-          </div>
-
-          <div className="__userData">
-            <div>
-              <h5>Soliu</h5>
-
-              <p>Soliu has been checked out</p>
-            </div>
-
-            <p>7 mins ago</p>
-          </div>
-
-        </div>
-
-        
-      </div> */}
 
       <div className="--flex-center __firstCon">
         <h4 className="__title">Recent Activity</h4>
